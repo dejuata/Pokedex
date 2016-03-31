1f10786be941fe2f1b3c19bdd0a4f36432cd42cc
Juan David <dejuata@hotmail.com>
2016-03-31 17:43:15 -0500

    agruegue css y modifique js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-//Definiendo atributos del pokemon
 function Pokemon(nombre,vida,ataque,tipo,imagen)
 {
 	this.nombre = nombre;
@@ -10,23 +9,35 @@ function Pokemon(nombre,vida,ataque,tipo,imagen)
 
 function cargarPokemon()
 {
-	//Variables que contienen ruta de imagenes de pokemon
-	var bulbasaur  = 'http://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png';
-	var charmander = 'http://assets.pokemon.com/assets/cms2/img/pokedex/detail/004.png';
-	var squirtle   = 'http://assets.pokemon.com/assets/cms2/img/pokedex/detail/007.png';
-	var pikachu	   = 'http://assets.pokemon.com/assets/cms2/img/pokedex/detail/025.png';
+	var url = 'http://assets.pokemon.com/assets/cms2/img/pokedex/detail/00';
+	var imagen = [];
+	var pokemon = [ ['Bulbasaur',200,30,'Planta'] , ['Ivysaur',300,40,'Planta']   , ['Venusaur',600,60,'Planta'] , 
+				    ['Charmander',200,30,'Fuego'] , ['Charmeleon',300,40,'Fuego'] , ['Charizard',600,60,'Fuego'] , 
+				    ['Squirtle',200,30,'Agua']    , ['Wartotle',300,40,'Agua']    , ['Blastoise',600,60,'Agua'] ];
 
-	//Creando el pokemon
-	var pokemon0 = new Pokemon('Bulbasaur', 150, 50, 'Planta', bulbasaur);
-	var pokemon1 = new Pokemon('Charmander', 100, 70, 'Fuego', charmander);
-	var pokemon2 = new Pokemon('Squirtle', 100, 70, 'Agua', squirtle);
-	var pokemon3 = new Pokemon('Pikachu', 90, 60, 'Electrico', pikachu);
+	for(var i = 1; i <= pokemon.length; i++)
+	{
+		imagen.push(url+i+'.png');		
+	}
+	for(var i = 0; i < pokemon.length; i++)
+	{
+		pokemon[i].push(imagen[i]);
+	}
 
-	var arreglo = [pokemon0,pokemon1,pokemon2,pokemon3];
+	var listado = [];	
+	var div = document.getElementById('marco');	
 
-	var div = document.getElementById('marco');
-	
-	for(var i = 0; i < arreglo.length ; i++ ){
+	for(var i = 0; i < pokemon.length; i++)
+	{
+		nombre 	= pokemon[i][0];
+		vida = pokemon[i][1];
+		ataque = pokemon[i][2];
+		tipo = pokemon[i][3];
+		imagen = pokemon[i][4];
+
+		var objeto = new Pokemon(nombre,vida,ataque,tipo,imagen);
+
+		listado.push(objeto);
 
 		var di  = document.createElement('div');
 		var h2	= document.createElement('h2');
@@ -34,13 +45,31 @@ function cargarPokemon()
 		var p 	= document.createElement('p');
 		var p1 	= document.createElement('p');
 		var p2 	= document.createElement('p');
+
+		h2.innerHTML= listado[i].nombre;
+		img.src = listado[i].imagen;
+		p.innerHTML = 'Vida = ' + listado[i].vida;
+		p1.innerHTML = 'Ataque = '+ listado[i].ataque;		
+		di.setAttribute('class','pokemon');
+		p2.innerHTML =  listado[i].tipo;
+
+		if(listado[i].tipo == 'Planta')
+		{
+			p2.setAttribute('class','planta');
+		}
+		else if(listado[i].tipo == 'Fuego')
+		{
+			p2.setAttribute('class','fuego');
+		}
+		else if(listado[i].tipo == 'Agua')
+		{
+			p2.setAttribute('class','agua');
+		}
+
+
+
+
 		
-		h2.innerHTML= arreglo[i].nombre;
-		img.src = arreglo[i].imagen;
-		p.innerHTML = 'Vida = ' + arreglo[i].vida;
-		p1.innerHTML = 'Ataque = '+ arreglo[i].ataque;
-		p2.innerHTML = 'Tipo = '+ arreglo[i].tipo;
-		di.setAttribute('class','pokemon')
 
 		div.appendChild(di);
 		di.appendChild(h2);
@@ -48,10 +77,11 @@ function cargarPokemon()
 		di.appendChild(p);
 		di.appendChild(p1);
 		di.appendChild(p2);
-
+		}
+		
+		
 	}
-	
-}
+
 
 
 
