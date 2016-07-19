cdfbb77a1a6a78b901a6bd37ea9f25d8390a77f1
Juan David <dejuata@hotmail.com>
2016-07-19 10:36:36 -0500

    Responsive Design

--- js/script.js
+++ js/script.js
@@ -9,15 +9,29 @@ function Pokemon(nombre,vida,ataque,tipo,imagen)
 
 function cargarPokemon()
 {
-	var url = 'http://assets.pokemon.com/assets/cms2/img/pokedex/detail/00';
+	var url = 'http://assets.pokemon.com/assets/cms2/img/pokedex/detail/0';
 	var imagen = [];
 	var pokemon = [ ['Bulbasaur',200,30,'Planta'] , ['Ivysaur',300,40,'Planta']   , ['Venusaur',600,60,'Planta'] , 
 				    ['Charmander',200,30,'Fuego'] , ['Charmeleon',300,40,'Fuego'] , ['Charizard',600,60,'Fuego'] , 
-				    ['Squirtle',200,30,'Agua']    , ['Wartotle',300,40,'Agua']    , ['Blastoise',600,60,'Agua'] ];
+				    ['Squirtle',200,30,'Agua']    , ['Wartotle',300,40,'Agua']    , ['Blastoise',600,60,'Agua'],
+				    ['Caterpie',100,10,'Bicho']   , ['Metapod',200,10,'Bicho']	  , ['Butterfree',300,30,'Bicho'],
+				    ['Weedle',200,20,'Veneno']    , ['Kakuna',300,30,'Veneno']    , ['Beedrill',500,50,'Veneno'],
+				    ['Pidgey',300,20,'Volador']   , ['Pidgeotto',500,40,'Volador'], ['Pidgeot',800,60,'Volador'] 
+				  ];
 
 	for(var i = 1; i <= pokemon.length; i++)
 	{
-		imagen.push(url+i+'.png');		
+		if (i <= 9) {
+			imagen.push(url+'0'+i+'.png');	
+		}
+		else{
+			if (i >= 10){
+				imagen.push(url+i+'.png');
+			}
+		}
+
+		
+				
 	}
 	for(var i = 0; i < pokemon.length; i++)
 	{
@@ -65,6 +79,18 @@ function cargarPokemon()
 		else if(listado[i].tipo == 'Agua')
 		{
 			p2.classList.add('agua','tipo')
+		}
+		else if(listado[i].tipo == 'Bicho')
+		{
+			p2.classList.add('bicho','tipo')
+		}
+		else if(listado[i].tipo == 'Veneno')
+		{
+			p2.classList.add('veneno','tipo')
+		}
+		else if(listado[i].tipo == 'Volador')
+		{
+			p2.classList.add('volador','tipo')
 		}
 
 		div.appendChild(di);
